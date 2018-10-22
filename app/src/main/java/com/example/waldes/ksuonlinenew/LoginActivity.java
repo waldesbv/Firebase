dd46200c10c2241e16835ff344a631f6e6b134be
Waldes <waldesbv777@gmail.com>
2018-10-23 02:17:03 +0300

    up

--- app/src/main/java/com/example/waldes/ksuonlinenew/LoginActivity.java
+++ app/src/main/java/com/example/waldes/ksuonlinenew/LoginActivity.java
@@ -1,12 +1,12 @@
 package com.example.waldes.ksuonlinenew;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,177 +14,41 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Iterator;
 
 public class LoginActivity extends AppCompatActivity {
 
-    //VIEWS AND WIDGET FIELDS
-    Button loginBtn;
-    EditText userEmailEdit, userPasswordEdit;
-
-    //String Fields
-    String userEmailString, userPasswordString;
-
-    //FIREBASE AUTHENTICATION FIELDS
-    FirebaseAuth mAuth;
-    FirebaseAuth.AuthStateListener mAuthListener;
-
-    DatabaseReference mDatabaseRef;
-
+    private EditText txtEmailLogin;
+    private EditText txtPwd;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
-        //ASSIGN ID'S
-        loginBtn = (Button) findViewById(R.id.buttonLogin);
-        userEmailEdit = (EditText) findViewById(R.id.editEmail);
-        userPasswordEdit = (EditText) findViewById(R.id.editPassword);
-
-
-        //ASSIGN INSTANCES
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Users");
-
-        mAuth = FirebaseAuth.getInstance();
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                if (user != null)
-                {
-
-                    final String emailForVer = user.getEmail();
-
-                    mDatabaseRef.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-
-                            checkUserValidation(dataSnapshot, emailForVer);
-
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
-                        }
-                    });
-
-                }else
-                {
-
-
-
-                }
-
-
-            }
-        };
-
-        //ON CLICK LISTENER
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //Perform login operation
-                userEmailString = userEmailEdit.getText().toString().trim();
-                userPasswordString = userPasswordEdit.getText().toString().trim();
-
-                if(!TextUtils.isEmpty(userEmailString) && !TextUtils.isEmpty(userPasswordString))
-                {
-
-                    mAuth.signInWithEmailAndPassword(userEmailString, userPasswordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-
-                            if(task.isSuccessful())
-                            {
-
-                                mDatabaseRef.addValueEventListener(new ValueEventListener() {
-                                    @Override
-                                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-                                           checkUserValidation(dataSnapshot, userEmailString);
-
-                                    }
-
-                                    @Override
-                                    public void onCancelled(DatabaseError databaseError) {
-
-                                    }
-                                });
-
-                            }
-                            else
-                            {
-                                Toast.makeText(LoginActivity.this, "User Login Failed" , Toast.LENGTH_LONG).show();
-                            }
-
-                        }
-                    });
-
-                }
-
-
-            }
-        });
-
-
-
+        txtEmailLogin = (EditText) findViewById(R.id.txtEmailLogin);
+        txtPwd = (EditText) findViewById(R.id.txtPasswordLogin);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
-    private void checkUserValidation(DataSnapshot dataSnapshot, String emailForVer) {
+    public void btnUserLogin_Click(View v) {
+        final ProgressDialog progressDialog = ProgressDialog.show(LoginActivity.this, "Please wait...", "Proccessing...", true);
 
-         Iterator iterator = dataSnapshot.getChildren().iterator();
+        (firebaseAuth.signInWithEmailAndPassword(txtEmailLogin.getText().toString(), txtPwd.getText().toString()))
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
 
-        while (iterator.hasNext())
-        {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_LONG).show();
+                            Intent i = new Intent(LoginActivity.this, ProfileActivity.class);
+                            i.putExtra("Email", firebaseAuth.getCurrentUser().getEmail());
+                            startActivity(i);
+                        } else {
+                            Log.e("ERROR", task.getException().toString());
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
 
-            DataSnapshot dataUser = (DataSnapshot) iterator.next();
-
-            if( dataUser.child("emailUser").getValue().toString().equals(emailForVer))
-            {
-
-                if (dataUser.child("isVerified").getValue().toString().equals("unverified"))
-                {
-
-
-
-                }else
-                {
-
-                    startActivity(new Intent(LoginActivity.this, Welcome.class));
-
-                }
-
-            }
-
-        }
-
-
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        mAuth.removeAuthStateListener(mAuthListener);
+                        }
+                    }
+                });
     }
 }
